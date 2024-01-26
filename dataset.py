eb8c4bc81327642771df4b7d5d1fb6976e0a11a3
Linkdom <donglinkang2021@163.com>
2024-01-26 10:59:42 +0800

    add dataset code

--- dataset.py
+++ dataset.py
@@ -59,90 +59,107 @@ def regress_gaussian(
 
     return x, y, label
 
-def classify_two_gauss_data(num_samples, noise):
-    points = []
-
-    variance = np.interp(noise, [0, 0.5], [0.5, 4])
+def classify_two_gauss_data(
+        num_samples:int, 
+        noise:float, 
+        radius:int = 6
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generates a dataset of points with two gaussian centers
+    """
+    variance = radius * noise + 0.5
+    n = num_samples // 2
 
     def gen_gauss(cx, cy, label):
-        for _ in range(num_samples // 2):
-            x = normal_random(cx, variance)
-            y = normal_random(cy, variance)
-            points.append({'x': x, 'y': y, 'label': label})
+        x = cx + np.sqrt(variance) * np.random.randn(n)
+        y = cy + np.sqrt(variance) * np.random.randn(n)
+        label = np.ones(n) * label
+        return x, y, label
 
-    gen_gauss(2, 2, 1)
-    gen_gauss(-2, -2, -1)
+    x_pos, y_pos, label_pos = gen_gauss(2, 2, 1)
+    x_neg, y_neg, label_neg = gen_gauss(-2, -2, -1)
 
-    return points
+    x = np.concatenate([x_pos, x_neg])
+    y = np.concatenate([y_pos, y_neg])
+    label = np.concatenate([label_pos, label_neg])
 
-def classify_spiral_data(num_samples, noise):
-    points = []
+    return x, y, label
+
+def classify_spiral_data(
+        num_samples:int, 
+        noise:float, 
+        radius:int = 6
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generates a dataset of points with two spiral data
+    """
     n = num_samples // 2
 
     def gen_spiral(delta_t, label):
-        for i in range(n):
-            r = i / n * 5
-            t = 1.75 * i / n * 2 * np.pi + delta_t
-            x = r * np.sin(t) + np.random.uniform(-1, 1) * noise
-            y = r * np.cos(t) + np.random.uniform(-1, 1) * noise
-            points.append({'x': x, 'y': y, 'label': label})
+        r = np.linspace(0.0, radius, n)
+        t = 1.75 * np.linspace(0.0, 2 * np.pi, n) + delta_t
+        x = r * np.sin(t) + np.random.uniform(-1, 1, n) * noise
+        y = r * np.cos(t) + np.random.uniform(-1, 1, n) * noise
+        label = np.ones(n) * label
+        return x, y, label
 
-    gen_spiral(0, 1)
-    gen_spiral(np.pi, -1)
+    x_pos, y_pos, label_pos = gen_spiral(0, 1)
+    x_neg, y_neg, label_neg = gen_spiral(np.pi, -1)
+    x = np.concatenate([x_pos, x_neg])
+    y = np.concatenate([y_pos, y_neg])
+    label = np.concatenate([label_pos, label_neg])
 
-    return points
+    return x, y, label
 
-def classify_circle_data(num_samples, noise):
-    points = []
-    radius = 5
+def classify_circle_data(
+        num_samples:int, 
+        noise:float, 
+        radius:int = 6
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generates a dataset of points with two circle data
+    """
+    n = num_samples // 2
 
-    def get_circle_label(p, center):
-        return 1 if dist(p, center) < (radius * 0.5) else -1
+    def gen_circle(min_r, max_r):
+        r = np.random.uniform(min_r, max_r, n)
+        t = np.random.uniform(0, 2 * np.pi, n)
+        x = r * np.sin(t)
+        y = r * np.cos(t)
+        noise_x = np.random.uniform(-radius, radius, n) * noise
+        noise_y = np.random.uniform(-radius, radius, n) * noise
+        tmp_xy = np.array([x + noise_x, y + noise_y]).T # (num_samples, 2)
+        distance = np.sqrt(np.sum(tmp_xy ** 2, axis=-1))
+        label = np.ones(n) * (distance < (radius * 0.5)) * 2 - 1
+        return x, y, label
+    
+    x_pos, y_pos, label_pos = gen_circle(0, radius * 0.5)
+    x_neg, y_neg, label_neg = gen_circle(radius * 0.7, radius)
+    x = np.concatenate([x_pos, x_neg])
+    y = np.concatenate([y_pos, y_neg])
+    label = np.concatenate([label_pos, label_neg])
 
-    for _ in range(num_samples // 2):
-        r = np.random.uniform(0, radius * 0.5)
-        angle = np.random.uniform(0, 2 * np.pi)
-        x = r * np.sin(angle)
-        y = r * np.cos(angle)
-        noise_x = np.random.uniform(-radius, radius) * noise
-        noise_y = np.random.uniform(-radius, radius) * noise
-        label = get_circle_label({'x': x + noise_x, 'y': y + noise_y}, {'x': 0, 'y': 0})
-        points.append({'x': x, 'y': y, 'label': label})
+    return x, y, label
 
-    for _ in range(num_samples // 2):
-        r = np.random.uniform(radius * 0.7, radius)
-        angle = np.random.uniform(0, 2 * np.pi)
-        x = r * np.sin(angle)
-        y = r * np.cos(angle)
-        noise_x = np.random.uniform(-radius, radius) * noise
-        noise_y = np.random.uniform(-radius, radius) * noise
-        label = get_circle_label({'x': x + noise_x, 'y': y + noise_y}, {'x': 0, 'y': 0})
-        points.append({'x': x, 'y': y, 'label': label})
+def classify_xor_data(
+        num_samples:int, 
+        noise:float, 
+        radius:int = 6,
+        padding:float = 0.3
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generates a dataset of points with two xor data
+    """
 
-    return points
+    x = np.random.uniform(-radius, radius, num_samples)
+    y = np.random.uniform(-radius, radius, num_samples)
+    x += ((x > 0) * 2 - 1) * padding
+    y += ((y > 0) * 2 - 1) * padding
+    noise_x = np.random.uniform(-radius, radius, num_samples) * noise
+    noise_y = np.random.uniform(-radius, radius, num_samples) * noise
+    label = ((x + noise_x) * (y + noise_y) >= 0) * 2 - 1
 
-def classify_xor_data(num_samples, noise):
-    def get_xor_label(p):
-        return 1 if p['x'] * p['y'] >= 0 else -1
+    return x, y, label
+    
+    
 
-    points = []
-
-    for _ in range(num_samples):
-        x = np.random.uniform(-5, 5)
-        padding = 0.3
-        x += padding if x > 0 else -padding
-        y = np.random.uniform(-5, 5)
-        y += padding if y > 0 else -padding
-        noise_x = np.random.uniform(-5, 5) * noise
-        noise_y = np.random.uniform(-5, 5) * noise
-        label = get_xor_label({'x': x + noise_x, 'y': y + noise_y})
-        points.append({'x': x, 'y': y, 'label': label})
-
-    return points
-
-def normal_random(mean=0, variance=1):
-    return mean + np.sqrt(variance) * np.random.randn()
-
-def dist(x1, y1, x2, y2):
-    dx, dy = x1 - x2, y1 - y2
-    return np.sqrt(dx*dx + dy*dy)
